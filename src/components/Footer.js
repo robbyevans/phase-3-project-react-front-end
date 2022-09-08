0e0b14177c289d17424334e4c7f97d2c0601cc5a
Evans Rutto <robbyevans001@gmail.com>
2022-09-08 22:55:00 +0300

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div id="contact" className="footer">
+       <a id="jumpHere"></a>
+
+<div className="container">
+  <div className="row">
+    <div className="footer-col-3">
+      <h3>Contacts</h3>
+      <p>We are located along Ngong Road Nairobi <br/> call us on</p>
+     <ul>
+      <li>+254720563260 Nairobi</li>
+      <li>+254780282847 Eldoret</li>
+
+      <li>email <a href="">robbyevans001@gmail.com</a>
+      </li>
+
+     </ul>
+    </div>
+    <div className="footer-col-3">
+      <h3>useful links</h3>
+     <ul>
+      <li>Coupons</li>
+      <li>Blog Post</li>
+      <li>Return Policy</li>
+      <li>Coupons</li>
+      <li>Be Affiliate</li>
+     </ul>
+    </div>
+    <div className="footer-col-4">
+      <h3>Follow us</h3>
+     <ul>
+      <li><a href="">Facebook</a></li>
+      <li><a href="">Twitter</a></li>
+      <li><a href="">Instagram</a></li>
+      <li><a href="">YouTube</a></li>
+     </ul>
+    </div>
+  </div>
+  
+  <p className="copyright" >copright@2022-Robbynet</p>
+</div>
+
+    </div>
+  )
+}
+
+export default Footer
