85c5c358c64a43797feb6231442dd89548a3add0
Evans Rutto <robbyevans001@gmail.com>
2022-09-08 22:55:17 +0300

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>Lyrical</h1>
+      <p>music is life</p>
+    </div>
+  )
+}
+
+export default Header
